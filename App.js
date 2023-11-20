365544f15e41428fc4e5f671beec60a662551df8
Sergio Scotta <sergeen@gmail.com>
2023-11-20 17:50:18 -0300

    Germination: basic counter that suvives blocking the phone

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, SafeAreaView } from 'react-native';
+import { useState, useEffect } from 'react';
 
 export default function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+  const [initalTime, setInitalTime] = useState(0);
+  const [text, setText] = useState('das');
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date().getTime());
+    }, 500);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Counter {Math.floor((currentTime - initalTime) / 1000)}</Text>
+      <SafeAreaView>
+        <Button title="Start counter" onPress={e => setInitalTime(new Date().getTime())}></Button>
+      </SafeAreaView>
       <StatusBar style="auto" />
     </View>
   );
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-My take on the timer app world
+## Admuti
+Advanced multy timer: My take on the timer app world
+
+#### Intention
+To have a tool for my day to day that helps me keeo track of different activities.
